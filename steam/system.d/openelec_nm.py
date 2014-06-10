0f0ea6d85d6e68603ab3680161aa725dd59b48f9
Lukas Rusak <lorusak@gmail.com>
2014-06-09 19:03:29 -0700

    Updated scripts. Stuck until dbus policy is changed :(

--- steam/system.d/openelec_nm.py
+++ steam/system.d/openelec_nm.py
@@ -1,10 +1,16 @@
-import dbus, glib
+import dbus, glib, dbus.service
 from dbus.mainloop.glib import DBusGMainLoop
 
-DBusGMainLoop(set_as_default=True)
-self.bus = dbus.SystemBus()
+class MyDBUSService(dbus.service.Object):
+  def __init__(self):
+    bus_name = dbus.service.BusName('org.freedesktop.NetworkManager', bus=dbus.SystemBus())
+    dbus.service.Object.__init__(self, dbus.SystemBus(), '/org/freedesktop/NetworkManager')
 
-self.bus.add_match_string("interface='org.freedesktop.Networkmanager', eavesdrop='true'")
+DBusGMainLoop(set_as_default=True)
+myservice = MyDBUSService()
+
+#bus = dbus.SystemBus()
+#bus.add_match_string("interface='org.freedesktop.Networkmanager'")
 
 mainloop = glib.MainLoop()
 mainloop.run()
